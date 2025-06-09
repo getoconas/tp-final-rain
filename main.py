5ff8d2dd49a44f90dd02c593222c2d372c94f048
getoconas <getoconas@gmail.com>
2025-06-09 11:56:47 -0300

    Modified main for test scrapers

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from scraping.somos_jujuy import get_news_somosjujuy
+from scraping.pagina_12 import get_news_pagina12
+from scraping.el_pais import get_news_elpais
+
 # Programa principal
+if __name__ == "__main__":
+  print(get_news_elpais("noria", 10))
+  
