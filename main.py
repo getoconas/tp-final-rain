1c9aebccb6fde4b6f805b57b1b52c48de13eb514
getoconas <getoconas@gmail.com>
2025-06-10 13:57:45 -0300

    Refactor call to scrapers

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from scraping.somos_jujuy import get_news_somosjujuy
-from scraping.pagina_12 import get_news_pagina12
-from scraping.el_pais import get_news_elpais
-from scraping.euronews import get_news_euronews
-from scraping.que_pasa_jujuy import get_news_quepasajujuy
+from scraping.pages.somos_jujuy import get_news_somosjujuy
+from scraping.pages.pagina_12 import get_news_pagina12
+from scraping.pages.el_pais import get_news_elpais
+from scraping.pages.euronews import get_news_euronews
+from scraping.pages.que_pasa_jujuy import get_news_quepasajujuy
+from scraping.pages.jujuy_dice import get_news_jujuydice
+from scraping.pages.tn import get_news_tn
 
 # Programa principal
 if __name__ == "__main__":
-  print(get_news_quepasajujuy("libertad", 10))
+  print(get_news_elpais("lento", 10))
   
