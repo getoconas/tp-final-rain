4e2c515a819cb0aa49c4c5a03594edbd1a71a050
getoconas <getoconas@gmail.com>
2025-06-14 19:08:39 -0300

    Add scrapers el_pais and pagina_12 for extract content news

---
+++ scraping/parsers/el_pais.py
@@ -0,0 +1,36 @@
+from scraping.utils.common import requests, BeautifulSoup, clean_text
+from scraping.utils.headers import get_random_headers
+
+def get_article_elpais(url):
+  response = requests.get(url, headers=get_random_headers())
+  if response.status_code != 200:
+    print(f"Error al obtener la noticia de 'el pais': {response.status_code}")
+    return []
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Titulo de la noticia
+  title = soup.find('h1', class_='a_t')
+  title = title.get_text(strip=True) if title else ''
+
+  # Resumen
+  summary = soup.find('h2', class_="a_st")
+  summary = summary.get_text(strip=True) if summary else ''
+
+  # Contenido
+  article_content = soup.find('div', class_='a_c clearfix')
+  content = []
+  if article_content:
+    for elem in article_content:
+      if elem.name == 'p':
+        text = elem.get_text()
+        if text:
+          content.append(text)
+  
+  text = summary + ' ' + '\n'.join(content)
+  text = clean_text(text)
+
+  return {
+    'titulo': title,
+    'contenido': text
+  }
---
+++ scraping/parsers/pagina_12.py
@@ -0,0 +1,42 @@
+from scraping.utils.common import requests, BeautifulSoup, clean_text
+from scraping.utils.headers import get_random_headers
+
+def get_article_pagina12(url):
+  response = requests.get(url, headers=get_random_headers())
+  if response.status_code != 200:
+    print(f"Error al obtener la noticia de 'pagina 12': {response.status_code}")
+    return []
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Titulo de la noticia
+  title = soup.find('h1')
+  title = title.get_text(strip=True) if title else ''
+
+  # Resumen
+  summary = soup.find('h2', class_="h3 ff-20px-w400")
+  summary = summary.get_text(strip=True) if summary else ''
+
+  # Contenido
+  article_content = soup.find('div', class_='section-2-col article-main-content')
+  content = []
+  if article_content:
+    article_text_div = article_content.find('div', class_='article-main-content article-text')
+    if article_text_div:
+      for elem in article_text_div:
+        if elem.name == 'p':
+          text = elem.get_text()
+          if text:
+            content.append(text)
+        if elem.name == 'h2':
+          text = elem.get_text(strip=True)
+          if text:
+            content.append(text + ' ')
+
+  text = summary + ' ' + '\n'.join(content)
+  text = clean_text(text)
+
+  return {
+    'titulo': title,
+    'contenido': text
+  }
