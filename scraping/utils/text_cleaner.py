ac7072af5a6e3325b0ff5b89ac5be7f01defc0f0
getoconas <getoconas@gmail.com>
2025-06-14 16:32:52 -0300

    Add function for clear text

---
+++ scraping/utils/text_cleaner.py
@@ -0,0 +1,6 @@
+import unicodedata
+
+def clean_text(text):
+  text = text.replace('\x93','"').replace('\x94','"').replace('\'','"').replace('\n',' ')
+  text = unicodedata.normalize("NFKC", text)
+  return text
