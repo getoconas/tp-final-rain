65f573736b27c9fd2a160d1cf408762590414fef
getoconas <getoconas@gmail.com>
2025-06-14 20:36:33 -0300

    Add scrapers for call to main

--- scraping/parsers/__init__.py
+++ scraping/parsers/__init__.py
@@ -1,9 +1,15 @@
 from .somos_jujuy import get_article_somosjujuy
 from .pagina_12 import get_article_pagina12
 from .el_pais import get_article_elpais
+from .euronews import get_article_euronews
+from .que_pasa_jujuy import get_article_quepasajujuy
+from .jujuy_dice import get_article_jujuydice
 
 ALL_ARTICLES = [
   get_article_somosjujuy,
   get_article_pagina12,
-  get_article_elpais
+  get_article_elpais,
+  get_article_euronews,
+  get_article_quepasajujuy,
+  get_article_jujuydice
 ]
