382039a97a3b941212a01f076a38e97a79cc478e
getoconas <getoconas@gmail.com>
2025-06-15 00:23:29 -0300

    Add scrapers efe, el_tribuno and wired

---
+++ scraping/parsers/el_tribuno.py
@@ -0,0 +1,45 @@
+from scraping.utils.common import requests, BeautifulSoup, clean_text
+from scraping.utils.headers import get_random_headers
+
+def get_article_eltribuno(url):
+  response = requests.get(url, headers=get_random_headers())
+  if response.status_code != 200:
+    print(f"Error al obtener la noticia de 'el tribuno': {response.status_code}")
+    return []
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Titulo de la noticia
+  title = soup.find('h1')
+  title = title.get_text(strip=True) if title else ''
+
+  # Resumen
+  summary = soup.find('div', class_="articulo__intro")
+  summary = summary.get_text(strip=True) if summary else ''
+
+  # Contenido
+  article_content = soup.find('div', attrs={'amp-access': 'mostrarNota'})
+  content = []
+  if article_content:
+    for elem in article_content:
+      if elem.name == 'p':
+        text = elem.get_text()
+        if text:
+          content.append(text)
+      elif elem.name == 'h2':
+        text = elem.get_text(strip=True)
+        if text:
+          content.append(text + ' ')
+      elif elem.name == 'ul':
+        for li in elem.find_all('li'):
+          li_text = li.get_text()
+          if li_text:
+            content.append('- ' + li_text)
+
+  text = summary + ' ' + '\n'.join(content)
+  text = clean_text(text)
+
+  return {
+    'titulo': title,
+    'contenido': text
+  }
---
+++ scraping/parsers/wired.py
@@ -0,0 +1,41 @@
+from scraping.utils.common import requests, BeautifulSoup, clean_text
+from scraping.utils.headers import get_random_headers
+
+def get_article_wired(url):
+  response = requests.get(url, headers=get_random_headers())
+  if response.status_code != 200:
+    print(f"Error al obtener la noticia de 'wired': {response.status_code}")
+    return []
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Titulo de la noticia
+  title = soup.find('h1')
+  title = title.get_text(strip=True) if title else ''
+
+  # Resumen
+  summary = soup.find('div', class_='ContentHeaderDek-bIqFFZ jTjywE')
+  summary = summary.get_text(strip=True) if summary else ''
+
+  # Contenido
+  article_content = soup.find_all('div', class_='BodyWrapper-kufPGa erMawQ body body__container article__body')
+  content = []
+  if article_content:
+    article_tag = soup.find('div', class_='body__inner-container')
+    for elem in article_tag:
+      if elem.name == 'p':
+        text = elem.get_text()
+        if text:
+          content.append(text)
+      elif elem.name == 'h2':
+        text = elem.get_text(strip=True)
+        if text:
+          content.append(text + ' ')
+  
+  text = summary + ' ' + '\n'.join(content)
+  text = clean_text(text)
+
+  return {
+    'titulo': title,
+    'contenido': text
+  }  
