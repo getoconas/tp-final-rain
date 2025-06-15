1bdbfb6122a9217d7b1e4655751c5194977dd11d
getoconas <getoconas@gmail.com>
2025-06-15 19:10:44 -0300

    Add replace in text_cleaner

--- scraping/utils/text_cleaner.py
+++ scraping/utils/text_cleaner.py
@@ -1,6 +1,6 @@
 import unicodedata
 
 def clean_text(text):
-  text = text.replace('\x93','"').replace('\x94','"').replace('\'','"').replace('“','"').replace('”','"').replace('\n',' ')
+  text = text.replace('\x93','"').replace('\x94','"').replace('\'','"').replace('“','"').replace('”','"').replace('«','"').replace('»','"').replace('\n',' ')
   text = unicodedata.normalize("NFKC", text)
   return text
