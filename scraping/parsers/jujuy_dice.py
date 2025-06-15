1f3ef10896cc9472f5070c6a82d90eb6fad98ab1
getoconas <getoconas@gmail.com>
2025-06-14 22:04:43 -0300

    Add scrapers tn and jujuy_dice

---
+++ scraping/parsers/jujuy_dice.py
@@ -0,0 +1,40 @@
+from scraping.utils.common import requests, BeautifulSoup, clean_text
+from scraping.utils.headers import get_random_headers
+
+def get_article_jujuydice(url):
+  response = requests.get(url, headers=get_random_headers())
+  if response.status_code != 200:
+    print(f"Error al obtener la noticia de 'jujuy dice': {response.status_code}")
+    return []
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Titulo de la noticia
+  title = soup.find('h1')
+  title = title.get_text(strip=True) if title else ''
+
+  # Resumen
+  summary_tag = soup.find('p', itemprop="description")
+  summary = ''
+  if summary_tag:
+    summary = summary_tag.find(text=True, recursive=False)
+    if summary:
+      summary = summary.strip()
+
+  # Contenido
+  article_content = soup.find('div', class_='cda')
+  content = []
+  if article_content:
+    for elem in article_content:
+      if elem.name == 'p':
+        text = elem.get_text()
+        if text:
+          content.append(text)
+    
+  text = summary + ' ' + '\n'.join(content)
+  text = clean_text(text)
+
+  return {
+    'titulo': title,
+    'contenido': text
+  }
---
+++ scraping/parsers/tn.py
@@ -0,0 +1,42 @@
+from scraping.utils.common import requests, BeautifulSoup, clean_text
+from scraping.utils.headers import get_random_headers
+
+def get_articles_tn(url):
+  response = requests.get(url, headers=get_random_headers())
+  if response.status_code != 200:
+    print(f"Error al obtener la noticia de 'tn': {response.status_code}")
+    return []
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Titulo de la noticia
+  title = soup.find('h1', class_='article__title font__display')
+  title = title.get_text(strip=True) if title else ''
+
+  # Resumen
+  summary = soup.find('h2', class_="article__dropline font__body")
+  summary = summary.get_text(strip=True) if summary else ''
+
+  # Contenido
+  article_content = soup.find('div', class_='default-article-color article__body article__article')
+  content = []
+  if article_content:
+    for elem in article_content:
+      if elem.name == 'p':
+        if elem.find('b') and 'Leé también:' in elem.find('b').get_text():
+          continue 
+        text = elem.get_text()
+        if text:
+          content.append(text)
+      elif elem.name == 'h2':
+        text = elem.get_text()
+        if text:
+          content.append(text + ' ')
+  
+  text = summary + ' ' + '\n'.join(content)
+  text = clean_text(text)
+
+  return {
+    'titulo': title,
+    'contenido': text
+  }        
