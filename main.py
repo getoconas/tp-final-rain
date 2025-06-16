46d58b1ff5ecb6e60caebb8e3d7fe706261d98ef
getoconas <getoconas@gmail.com>
2025-06-16 16:30:48 -0300

    Program principal

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from scraping.runner import get_all_news, fetch_articles_content
+from interface.app import NewsApp
+import tkinter as tk
 
-# Programa principal
 if __name__ == "__main__":
-  query = "eclipse"
-  max_news = 10
-
-  print("📡 Obteniendo enlaces de noticias...")
-  news = get_all_news(query, max_news)
-
-  print(f"\n🔎 Total noticias encontradas: {len(news)}. Comenzando la extracción de contenido...\n")
-  all_articles = fetch_articles_content(news)
-
-  print(f"\n✅ Artículos completos extraídos: {len(all_articles)}")
-
-  if all_articles:
-    print(all_articles)
-  
+  root = tk.Tk()
+  app = NewsApp(root)
+  root.mainloop()
