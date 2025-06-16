86dad1a22eef60636d574994a84c05622e35068f
getoconas <getoconas@gmail.com>
2025-06-16 17:49:54 -0300

    Refactor to replace text

--- scraping/utils/text_cleaner.py
+++ scraping/utils/text_cleaner.py
@@ -1,6 +1,15 @@
 import unicodedata
+import re
 
 def clean_text(text):
-  text = text.replace('  ',' ').replace('   ','').replace('“','"').replace('”','"').replace('«','"').replace('»','"').replace('\x93','"').replace('\x94','"').replace('\'','"').replace('\n',' ')
+  text = re.sub(r'\s+', ' ', text) # Reemplaza uno o más espacios/saltos de línea por un solo espacio
+
+  # Patrones para reemplazar por comillas dobles
+  quote_patterns = ['‘', '’', '“', '”', '«', '»', '\x93', '\x94', '\'']
+  text = re.sub(r'|'.join(map(re.escape, quote_patterns)), '"', text)
+
+  # Normalización Unicode para acentos
   text = unicodedata.normalize("NFKC", text)
-  return text
+  text = ''.join(c for c in text if not unicodedata.category(c) == 'Mn')
+  
+  return text.strip() # Añadir .strip() para limpiar espacios al principio/final
